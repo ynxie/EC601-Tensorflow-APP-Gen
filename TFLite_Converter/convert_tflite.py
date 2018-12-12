288b208b218a3ecfaff24d54c19fbc4b11ab2bff
JoranLau <43103125+JoranLau@users.noreply.github.com>
2018-12-12 18:56:14 -0500

    Add files via upload

---
+++ TFLite_Converter/convert_tflite.py
@@ -0,0 +1,11 @@
+
+import tensorflow as tf
+
+graph_def_file = "/home/ece-student/Desktop/pb_converted/output_graph.pb"
+input_arrays = ["Placeholder"]
+output_arrays = ["final_result"]
+
+converter = tf.lite.TFLiteConverter.from_frozen_graph(
+  graph_def_file, input_arrays, output_arrays)
+tflite_model = converter.convert()
+open("converted_model.tflite", "wb").write(tflite_model)
